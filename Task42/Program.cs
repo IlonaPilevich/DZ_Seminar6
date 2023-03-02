8ef00b24d7dd24a4748bffaa4ae92ef7d6cb9088
Ilona <ppsport88@mail.ru>
2023-03-02 19:38:43 +0300

    Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+
+int GetNumberConsole(string message)
+{
+    int number = 0;
+    Console.Write(message);
+    string? strValue = Console.ReadLine();
+    if (!int.TryParse(strValue, out number))
+    {
+        Console.WriteLine("Введите числовое значение!");
+        Environment.Exit(-1);
+    }
+    return number;
+}
+
+int number = GetNumberConsole("Введите десятичное числовое значение: ");
+
+string binLine = Convert.ToString(number, 2);
+Console.WriteLine($"Бинарный формат числа {number} = {binLine}");
